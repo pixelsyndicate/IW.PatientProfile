9747802f5e02969ac133a3f765f81493ff284cd0
Wil Dobson <wdobson@pixelsyndicate.com>
2014-11-19 13:15:00 -0800

    parameterizing stuff

--- controller/PatientController.js
+++ controller/PatientController.js
@@ -1,5 +1,5 @@
-        myApp.controller('PatientController', ['$scope', 'PatientModel',
-            function ($scope, PatientModel) {
-                var patients = PatientModel.getPatients();
-                $scope.patients = patients;
-            }]);
+myApp.controller('PatientController', ['$scope', 'PatientModel',
+    function ($scope, PatientModel) {
+        var patients = PatientModel.getPatients();
+        $scope.patients = patients;
+    }]);
--- model/PatientModel.js
+++ model/PatientModel.js
@@ -1,31 +1,22 @@
-        myApp.service('PatientModel', function () {
-            this.getPatients = function () {
-                return [
-                    {
-                        id: 0,
-                        fName: 'Shelby'
-                }, {
-                        id: 1,
-                        fName: 'Spencer'
-                }, {
-                        id: 2,
-                        fName: 'Hunter'
-                }, {
-                        id: 3,
-                        fName: 'Jorden'
-                }
-                ];
-            };
-        });
-
-
-
-        myApp.service('DetailModel', function () {
-            var thisDetails = getDetailsForPatient(var id);
-
-            function getDetailsForPatient(id) {
-
-                return [];
-            }
-
-        });
+myApp.service('PatientModel', function () {
+    this.getPatients = function () {
+        return [
+            {
+                id: 0,
+                fName: 'Shelby',
+                bDate: ''
+            }, {
+                id: 1,
+                fName: 'Spencer',
+                bDate: ''
+            }, {
+                id: 2,
+                fName: 'Hunter',
+                bDate: ''
+            }, {
+                id: 3,
+                fName: 'Jorden',
+                bDate: ''
+            }];
+    };
+});
